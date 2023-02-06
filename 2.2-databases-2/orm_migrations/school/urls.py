6659695abe837dd29f14ad528b66d95b3ae2c35b
Mariya Zamyatina <maria.net@mail.ru>
2023-02-06 18:52:15 +0100

    fix hw

--- 2.2-databases-2/orm_migrations/school/urls.py
+++ 2.2-databases-2/orm_migrations/school/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 
-from school.views import students_list, teachers_list
+from school.views import students_list
 
 urlpatterns = [
     path('', students_list, name='students'),
-    path('teachers', teachers_list, name='teachers'),
 ]
--- 2.2-databases-2/orm_migrations/school/views.py
+++ 2.2-databases-2/orm_migrations/school/views.py
@@ -5,19 +5,10 @@ from .models import Student, Teacher
 
 
 def students_list(request):
-    template = 'school/students_list.html'
+    template = 'school/base.html'
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
     ordering = 'group'
     students = Student.objects.prefetch_related('teachers').order_by(ordering)
-    context = { 'students': students}
-    return render(request, template, context)
-
-def teachers_list(request):
-    template = 'school/teachers_list.html'
-    # используйте этот параметр для упорядочивания результатов
-    # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
-    ordering = 'name'
-    teachers = Teacher.objects.prefetch_related('students').order_by(ordering)
-    context = { 'teachers': teachers}
+    context = {'students': students}
     return render(request, template, context)
